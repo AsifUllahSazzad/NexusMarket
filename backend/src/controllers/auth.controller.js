2c05c627811e478567b2b189c5b75312b8d531d5
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-09-23 03:28:31 +0600

    Refactor auth routes to use controller functions

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const postMerchant = (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  res.send();
+};
+
+export const postCustomer = (req, res) => {
+  const body = req.body;
+  console.log(body);
+
+  res.send();
+};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,18 +1,9 @@
 import express from "express";
+import { postCustomer, postMerchant } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.post("/register/merchant", (req, res) => {
-  const body = req.body;
-  console.log(body);
+router.post("/register/merchant", postMerchant);
 
-  res.send();
-});
-
-router.post("/register/customer", (req, res) => {
-  const body = req.body;
-  console.log(body);
-
-  res.send();
-});
+router.post("/register/customer", postCustomer);
 
 export default router;
